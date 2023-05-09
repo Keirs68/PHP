436634d56f9b60a7d5f9894f89ffc67110f19ae4
Keirs68 <putrimaulida1070@gmail.com>
2023-05-10 01:41:52 +0700

    Up Tugas 11

--- Tugas 11/pesanan_detail.php
+++ Tugas 11/pesanan_detail.php
@@ -10,26 +10,26 @@ $pesanan = $model->getPesanan($id);
     <div class="card mb-4">
         <div class="card-body">
             <div class="card-header">
-                <div>
-                    <h4>Detail Pesanan</h4>
-                    <h4>Nama Pemesan: <?= $pesanan["nama_pelanggan"] ?></h4>
-                    <br>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Tanggal</th>
-                                <th>Total</th>
-                                <th>Pelanggan Id</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td><?= $pesanan["tanggal"] ?> </td>
-                                <td><?= $pesanan["total"] ?> </td>
-                                <td><?= $pesanan["pelanggan_id"] ?> </td>
-                        </tbody>
-                    </table>
-                </div>
+
+                <h4>Detail Pesanan</h4>
+                <h4>Nama Pemesan: <?= $pesanan["nama_pelanggan"] ?></h4>
+                <br>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Tanggal</th>
+                            <th>Total</th>
+                            <th>Pelanggan Id</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><?= $pesanan["tanggal"] ?> </td>
+                            <td><?= $pesanan["total"] ?> </td>
+                            <td><?= $pesanan["pelanggan_id"] ?> </td>
+                    </tbody>
+                </table>
+
             </div>
         </div>
     </div>
