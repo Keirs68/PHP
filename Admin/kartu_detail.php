f2afb04f2ccb22d973e385d5c5ca141cf80c9cfd
Keirs68 <putrimaulida1070@gmail.com>
2023-05-10 01:44:05 +0700

    Update

--- Admin/kartu_detail.php
+++ Admin/kartu_detail.php
@@ -10,29 +10,27 @@ $kartu = $model->getKartu($id);
     <div class="card mb-4">
         <div class="card-body">
             <div class="card-header">
-                <div>
-                    <h3>Detail Kartu</h3>
-                    <br>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Kode</th>
-                                <th>Nama</th>
-                                <th>Diskon</th>
-                                <th>Iuran</th>
+                <h3>Detail Kartu</h3>
+                <br>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Kode</th>
+                            <th>Nama</th>
+                            <th>Diskon</th>
+                            <th>Iuran</th>
 
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td><?= $kartu["kode"] ?> </td>
-                                <td><?= $kartu["nama"] ?> </td>
-                                <td><?= $kartu["diskon"] ?> </td>
-                                <td><?= $kartu["iuran"] ?> </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><?= $kartu["kode"] ?> </td>
+                            <td><?= $kartu["nama"] ?> </td>
+                            <td><?= $kartu["diskon"] ?> </td>
+                            <td><?= $kartu["iuran"] ?> </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
--- Admin/pelanggan_detail.php
+++ Admin/pelanggan_detail.php
@@ -10,34 +10,33 @@ $pelanggan = $model->getPelanggan($id);
     <div class="card mb-4">
         <div class="card-body">
             <div class="card-header">
-                <div>
-                    <h3>Detail Pelanggan</h3>
-                    <br>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Kode</th>
-                                <th>Nama</th>
-                                <th>Jenis Kelamin</th>
-                                <th>Tempat Lahir</th>
-                                <th>Tanggal Lahir</th>
-                                <th>Email</th>
-                                <th>Kartu Id</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td><?= $pelanggan["kode"] ?> </td>
-                                <td><?= $pelanggan["nama_pelanggan"] ?> </td>
-                                <td><?= $pelanggan["jk"] ?> </td>
-                                <td><?= $pelanggan["tmp_lahir"] ?> </td>
-                                <td><?= $pelanggan["tgl_lahir"] ?> </td>
-                                <td><?= $pelanggan["email"] ?> </td>
-                                <td><?= $pelanggan["kartu_id"] ?> </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+
+                <h3>Detail Pelanggan</h3>
+                <br>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Kode</th>
+                            <th>Nama</th>
+                            <th>Jenis Kelamin</th>
+                            <th>Tempat Lahir</th>
+                            <th>Tanggal Lahir</th>
+                            <th>Email</th>
+                            <th>Kartu Id</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><?= $pelanggan["kode"] ?> </td>
+                            <td><?= $pelanggan["nama_pelanggan"] ?> </td>
+                            <td><?= $pelanggan["jk"] ?> </td>
+                            <td><?= $pelanggan["tmp_lahir"] ?> </td>
+                            <td><?= $pelanggan["tgl_lahir"] ?> </td>
+                            <td><?= $pelanggan["email"] ?> </td>
+                            <td><?= $pelanggan["kartu_id"] ?> </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
