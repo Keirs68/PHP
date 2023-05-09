91afef64140afeb9907d40de04a2463f501132eb
Keirs68 <putrimaulida1070@gmail.com>
2023-05-10 01:34:37 +0700

    Tugas Pertemuan 11

--- Tugas 11/kartu_detail.php
+++ Tugas 11/kartu_detail.php
@@ -4,6 +4,36 @@ $model = new Kartu();
 $kartu = $model->getKartu($id);
 ?>
 
-<div>
-    <h3><?= $kartu['kode']?></h3>
-</div>
+<br>
+<br>
+<center>
+    <div class="card mb-4">
+        <div class="card-body">
+            <div class="card-header">
+                <div>
+                    <h3>Detail Kartu</h3>
+                    <br>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Kode</th>
+                                <th>Nama</th>
+                                <th>Diskon</th>
+                                <th>Iuran</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><?= $kartu["kode"] ?> </td>
+                                <td><?= $kartu["nama"] ?> </td>
+                                <td><?= $kartu["diskon"] ?> </td>
+                                <td><?= $kartu["iuran"] ?> </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</center>
--- Tugas 11/pesanan_detail.php
+++ Tugas 11/pesanan_detail.php
@@ -4,6 +4,33 @@ $model = new Pesanan();
 $pesanan = $model->getPesanan($id);
 ?>
 
-<div>
-    <h3><?= $pesanan['pelanggan_id'] ?></h3>
-</div>
+<br>
+<br>
+<center>
+    <div class="card mb-4">
+        <div class="card-body">
+            <div class="card-header">
+                <div>
+                    <h4>Detail Pesanan</h4>
+                    <h4>Nama Pemesan: <?= $pesanan["nama_pelanggan"] ?></h4>
+                    <br>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Tanggal</th>
+                                <th>Total</th>
+                                <th>Pelanggan Id</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><?= $pesanan["tanggal"] ?> </td>
+                                <td><?= $pesanan["total"] ?> </td>
+                                <td><?= $pesanan["pelanggan_id"] ?> </td>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</center>
